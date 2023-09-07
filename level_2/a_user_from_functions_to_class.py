0db0d7ee517e696dee98c1420defa4b83fdfd6af
MagerOK <farmarker@gmail.com>
2023-09-07 15:41:52 +0200

    level 2 fixes

--- level_2/a_user_from_functions_to_class.py
+++ level_2/a_user_from_functions_to_class.py
@@ -5,14 +5,19 @@
     1. Создайте класс User и перенесите всю логику работы с пользователем туда.
 """
 
-
-def make_username_capitalized(username: str):
-    return username.capitalize()
-
-
-def generate_short_user_description(username: str, user_id: int, name: str):
-    return f'User with id {user_id} has {username} username and {name} name'
-
-
 class User:
-    pass  # код писать тут
+    def __init__(self, user_id: int, username: str, name: str):
+        self.user_id = user_id
+        self.username = username
+        self.name = name
+
+    def make_username_capitalized(self):
+        return self.username.capitalize()
+
+    def __str__(self):
+        return f'User with id {self.user_id} has {self.username} username and {self.name} name'
+    
+if __name__ == '__main__':
+    person = User(1, 'billiejinn', 'pavel')
+    print(person.make_username_capitalized())
+    print(person)
--- level_2/b_user_should_be_banned.py
+++ level_2/b_user_should_be_banned.py
@@ -11,4 +11,19 @@ SURNAMES_TO_BAN = ['Vaughn', 'Wilhelm', 'Santaros', 'Porter', 'Smith']
 
 
 class User:
-    pass  # код писать тут
+    def __init__(self, name: str, surname: str, age: int):
+        self.name = name
+        self.surname = surname
+        self.age = age
+    
+    def should_be_banned(self):
+        return self.surname in SURNAMES_TO_BAN
+    
+    def __str__(self):
+        return f'User(name={self.name}, surname={self.surname}, age={self.age})'
+
+if __name__ == '__main__':
+    person_blacklist = User('Adolf', 'Vaughn', 30)
+    person_whitelist = User('Pavel', 'Mager', 24)
+    print(person_blacklist.should_be_banned())
+    print(person_whitelist.should_be_banned())
