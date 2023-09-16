87a03b3b83d7ea882d59388a0220f614d83f799e
Mischa <mishayartsev@gmail.com>
2023-09-16 21:31:00 +0300

    +level 2 challenges

--- level_2/b_user_should_be_banned.py
+++ level_2/b_user_should_be_banned.py
@@ -7,8 +7,17 @@
        Пользователя стоит забанить, если его фамилия находится в SURNAMES_TO_BAN.
 """
 
+from a_user_from_functions_to_class import User
+
+
 SURNAMES_TO_BAN = ['Vaughn', 'Wilhelm', 'Santaros', 'Porter', 'Smith']
 
 
-class User:
-    pass  # код писать тут
+class CheckUser(User):
+    def __init__(self, username: str, user_id: int, name: str, surname: str, age: int) -> None:
+        super().__init__(username, user_id, name)
+        self.surname = surname
+        self.age = age
+
+    def should_be_banned(self) -> bool:
+        return self.surname in SURNAMES_TO_BAN
