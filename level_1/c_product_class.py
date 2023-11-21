f8628d62c728efc8086f3b90867d8add838795ad
Aleksandr <aleksandr.kurov@gmail.com>
2023-11-21 23:25:34 +0300

    Add product

--- level_1/c_product_class.py
+++ level_1/c_product_class.py
@@ -9,8 +9,20 @@
 
 
 class Product:
-    pass  # код писать тут
+    def __init__(self, title: str, description: str, price: int, weight: float):
+        self.title = title
+        self.description = description
+        self.price = price
+        self.weight = weight
+
+    def __str__(self):
+        return (f'Информация о продукте: {self.title}\n'
+                f'{self.description}.\n{(self.price / 100):0.2f} ₽, {self.weight} г.')
 
 
 if __name__ == '__main__':
-    pass  # код писать тут
+    product = Product('Пломбир «Из Лавки»',
+                      ('К классическому сливочному пломбиру добавили солёную'
+                       'карамель, сваренную по рецепту французских кондитеров'),
+                      8900, 75)
+    print(product)
