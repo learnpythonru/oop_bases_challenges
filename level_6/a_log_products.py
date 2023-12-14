66ee16bc69eb03520969fac3dff197170225d56a
Beloded1 <alexandrbeloded@gmail.com>
2023-12-14 15:00:09 +0400

    'a' done

--- level_6/a_log_products.py
+++ level_6/a_log_products.py
@@ -19,24 +19,38 @@ class Product:
         return f'Product {self.title} with price {self.price}'
 
 
-class PremiumProduct(Product):
+class PrintLoggerMixin:
+    def log(self, message: str) -> None:
+        print(message)
+
+
+class PremiumProduct(PrintLoggerMixin, Product):
     def increase_price(self):
         self.price *= 1.2
+        self.log(f'Increased price for PremiumProduct: {self.price}')
 
     def get_info(self):
         base_info = super().get_info()
+        self.log(f'Getting info for PremiumProduct: {base_info}')
         return f'{base_info} (Premium)'
 
 
-class DiscountedProduct(Product):
+class DiscountedProduct(PrintLoggerMixin, Product):
     def decrease_price(self):
         self.price /= 1.2
+        self.log(f'Decrease price for DiscountedProduct: {self.price}')
 
     def get_info(self):
         base_info = super().get_info()
+        self.log(f'Getting info for DiscountedProduct: {base_info}')
         return f'{base_info} (Discounted)'
 
 
 if __name__ == '__main__':
-    pass
+    premium_product = PremiumProduct(title='Whiskey', price=5000.0)
+    premium_product.increase_price()
+    premium_product.get_info()
 
+    discount_product = DiscountedProduct(title='Vodka', price=450.0)
+    discount_product.decrease_price()
+    discount_product.get_info()
