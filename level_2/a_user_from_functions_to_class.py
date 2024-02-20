b503575116ebafdd571171b6231f9cd6573a86d1
Sergey Kuzmin <s.kuzmin@mail.ru>
2024-02-20 11:17:30 +0800

    level_2 homework

--- level_2/a_user_from_functions_to_class.py
+++ level_2/a_user_from_functions_to_class.py
@@ -15,4 +15,13 @@ def generate_short_user_description(username: str, user_id: int, name: str):
 
 
 class User:
-    pass  # код писать тут
+    def __init__(self, username: str, user_id: int, name: str) -> None:
+        self.username = username
+        self.user_id = user_id
+        self.name = name
+
+    def make_username_capitalized(self) -> str:
+        return self.username.capitalize()
+    
+    def generate_short_user_description(self) -> str:
+        return f'User with id {self.user_id} has {self.username} username and {self.name} name'
